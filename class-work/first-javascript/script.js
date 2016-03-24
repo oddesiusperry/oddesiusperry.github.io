978d8abff25ce73991fb1f7d0c96abbb69d6da18
oddesiusperry <Ayumi@AYUMI-HIRAYAMAs-MacBook-Pro.local>
2016-03-24 15:01:47 -0400

    startup-matchmaker-homework!

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,21 @@
+// alert('this is an alert. this is annoying');
+// 
+// document.write('writing to the document, like a boss');
+
+var num1 = 10 * 10;
+var num2 = 100/10;
+var num3 = 50-17;
+var num4 = 50+17;
+var myName = 'Oddesius Perry'
+
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(myName);
+
+document.getElementById('title').style.color='red';
+
+document.getElementById('text-chunk').style.background='blue';
+
+document.getElementById('text-chunk').style.color='white';
