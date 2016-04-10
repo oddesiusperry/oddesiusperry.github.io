c05e28399b2151b06bc86ac654dc463a2c41953a
oddesiusperry <Ayumi@AYUMI-HIRAYAMAs-MacBook-Pro.local>
2016-04-10 15:48:10 -0400

    temp_converter!

---
+++ homework/temp-converter/js/app.js
@@ -0,0 +1,18 @@
+$(function() {
+  $('.inputTemp').on('keypress', function (){
+    var tempId = $(this).attr('data-tempid');
+     if (this) {
+
+     }
+     else {
+
+     }
+
+  });
+});
+
+
+
+
+
+
