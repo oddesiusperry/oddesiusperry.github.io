0965e1c52e9f980fcbe02ac4a6c5e133b0d4c853
oddesiusperry <Ayumi@AYUMI-HIRAYAMAs-MacBook-Pro.local>
2016-04-10 20:04:45 -0400

    work!

--- homework/temp-converter/js/app.js
+++ homework/temp-converter/js/app.js
@@ -1,18 +1,38 @@
 $(function() {
-  $('.inputTemp').on('keypress', function (){
-    var tempId = $(this).attr('data-tempid');
-     if (this) {
 
+  $('.inputTemp').on('keypress', function () {
+    var tempId = $(this).attr('data-tempid'); 
+
+    if (tempId == "Fahr") {
+      $(this).value (* 1.8) + 32;
      }
-     else {
-
+     else if { 
+      (tempId == "Cels") 
+      $(this).value (- 32) * 1.8;
+     }
+     $(tempId).value = () {
      }
 
-  });
+     //function C to F
+    var $f = function CtoF() {
+    
+    if (f <= 32 ) { //color blue for cold
+      $('#box').css('backgroundColor', '#0D47A1');
+    } 
+    else if (c > 32 || c < 70) { //orange for warm
+      $('#box').css('backgroundColor', '#FF5722');
+    } 
+    else if (c > 70) { //red for hot
+      $('#box').css('backgroundColor', '#B71C1C');
+    } 
+    else {
+      $('#box').css('backgroundColor', '#EEEEEE');
+    }
+
+    $('.inputTemp').value = !isNaN(tempId) ? parseFloat(tempId.toFixed(1)) : "Err"
+
+    });
 });
-
-
-
 
 
 
